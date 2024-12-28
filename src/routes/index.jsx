9e14520d74c0c1b26cf12ad8f43845a87f426dab
Moutou Soufiane <forworksoufiane@gmail.com>
2024-12-28 17:39:44 +0100

    Add files via upload

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+              <h1>A Contacts App Worcked With React Router.</h1>
+              <h3>More Faitures Coming Soon!</h3>
+              
+              </p>
+    );
+  }
+  
